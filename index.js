ceeaeec6563a18bd273839c70893d3dba7cc2309
guillaume.anton@ynov.com <guillaume.anton@ynov.com>
2020-02-17 09:10:20 +0100

    complet index.js

--- index.js
+++ index.js
@@ -1,44 +1,117 @@
-var express = require('express'); 
-var hostname = 'localhost'; 
-var port = 8080; 
-var app = express(); 
-var myRouter = express.Router();
+// var express = require('express'); 
+// const mysql = require('mysql');
+// var hostname = 'localhost'; 
+// var port = 8080; 
+// var app = express(); 
+// var myRouter = express.Router();
 
-var bodyParser = require("body-parser"); 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
- 
+// var bodyParser = require("body-parser"); 
+// var passport = require("passport");
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-myRouter.route('/peche/name')
-// GET
-.get(function(req,res){ 
-	  res.json({message : "Carnet de pêche", methode : req.method});
-})
-//POST
-app.post('/peche/name/add', function(req,res){
-      res.json({message : "Ajout d'un nouveau poisson" , espece : req.body.espece, nom : req.body.nom, methode : req.methode});
-})
-//PUT
-app.put('/peche/name/update', function(req,res){ 
-      res.json({message : "Mise à jour des informations de votre poisson", methode : req.method});
-})
-//DELETE
-app.delete('/peche/name/delete', function(req,res){ 
-res.json({message : "Suppression d'un poisson", nom: req.body.nom, methode : req.method});  
-}); 
+// var db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "api_poisson",
+//   port: "8889"
+// });
 
-//LOGIN
-app.post('/login',
-  passport.authenticate('local'),
-  function(req, res) {
-    res.json({message : "Espace de connection"})
-    res.redirect('/peche/' + req.user.username);
-  });
+// db.connect(function(err) {
+//   if(err) throw err;
+//   console.log("Connected!");
+// });
+
+// myRouter.route('/peche/poissons')
+// // GET ALL
+//     .get(function(req,res){ 
+//         db.query('SELECT * FROM poisson'), (err, result) => {
+//             if (err) {
+//                 res.json(error(err.message))
+//                 console.log('Pas de réponse')
+//             } else {
+//                 res.json(success(result))
+//                 console.log('Voici les poissons')
+//             }
+//         };
+//     })
+
+// myRouter.route('/peche/poisson/add')
+// //POST Ajouter 
+// .post(function(req,res){
+//     db.query('INSERT INTO poisson (nom, espece, taille, poids, lieu, date) VALUES(?,?,?,?,?,?)'), [nom, espece, taille, poids, lieu, date], (err, result) => {
+//         if (err) {
+//             res.json(error(err.message))
+//         } else {
+//             res.json(success(result))
+//         }
+//     };
+// })
+
+// myRouter.route('/peche/poisson/update:id')
+// //PUT
+// app.put(function(req,res){ 
+//       res.json({message : "Mise à jour des informations de votre poisson", methode : req.method});
+//       db.query("UPDATE poisson SET nom = 'req.body.nom' , espece = 'req.body.espece' , taille = 'req.body.taille' , poids = 'req.body.poids' , lieu = 'req.body.lieu' , date = 'req.body.lieu' WHERE id=req.id")
+// })
+// //DELETE
+// app.delete('/peche/poisson/delete:id', function(req,res){ 
+// res.json({message : "Suppression d'un poisson", nom: req.body.nom, methode : req.method});  
+// }); 
  
-// Nous demandons à l'application d'utiliser notre routeur
-app.use(myRouter);  
+// // Nous demandons à l'application d'utiliser notre routeur
+// app.use(myRouter);  
  
-// Démarrer le serveur 
-app.listen(port, hostname, function(){
-	console.log("Mon serveur fonctionne sur http://"+ hostname +":"+port); 
+// // Démarrer le serveur 
+// app.listen(port, hostname, function(){
+// 	console.log("Mon serveur fonctionne sur http://"+ hostname +":"+port); 
+// });
+
+// app.use(myRouter)
+
+// app.listen(console.log(`Ecoute sur le port : ${port}`)) 
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+//jwt stuff
+const jwt = require("jsonwebtoken");
+
+//passport stuff
+const passport = require("passport");
+const jwtStrategry  = require("./config/jwt")
+passport.use(jwtStrategry);
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get("/", (req, res) => {
+    res.send("hello express server")
+})
+
+app.post("/login", (req, res) => {
+    let { email, password } = req.body;
+    //This lookup would normally be done using a database
+    if (email === "") {
+        if (password === "") { //the password compare would normally be done using bcrypt.
+            const opts = {}
+            opts.expiresIn = 120;  //token expires in 2min
+            const secret = "SECRET_KEY" //normally stored in process.env.secret
+            const token = jwt.sign({ email }, secret, opts);
+            return res.status(200).json({
+                message: "Auth Passed",
+                token
+            })
+        }
+    }
+    return res.status(401).json({ message: "Auth Failed" })
 });
+
+app.get("/protected", passport.authenticate('jwt', { session: false }), (req, res) => {
+    return res.status(200).send("YAY! this is a protected Route")
+})
+
+app.listen(3001);
